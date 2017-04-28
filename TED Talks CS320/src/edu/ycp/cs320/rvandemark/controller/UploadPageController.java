573f5bfa62689c99877fa71113e0720e4ed5ef23
Joshua DeBose <jdebose@ycp.edu>
2017-04-28 19:32:57 -0400

    will it work?

--- TED Talks CS320/src/edu/ycp/cs320/rvandemark/controller/UploadPageController.java
+++ TED Talks CS320/src/edu/ycp/cs320/rvandemark/controller/UploadPageController.java
@@ -5,6 +5,7 @@ import edu.ycp.cs320.rvandemark.model.Engine;
 public class UploadPageController {
 	
 	//Ezzypoo Wuz Here
+	//Spankbank n chill
 
 	public boolean insertVideo(String url, String e_url, String name, String speaker, String thumb, int tr, int nr, double rating, int uM, int uD, int uY, String dLine) {
 		//System.out.println(url + " made it here too!");          Test line
